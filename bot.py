f1ff3b584c02c18ad52bc1ea3faa02459d70a10a
Keynes11552432123 <80331285+Keynes11552432123@users.noreply.github.com>
2022-02-25 21:42:03 +0800

    Version2.0

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import discord
 from discord.ext import commands
+import json
+
+with open ('setting.json',mode='r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 
 intents = discord.Intents.default()
 intents.members = True
@@ -12,18 +16,18 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(880437833641394267)
+    channel = bot.get_channel(int(jdata['Welcome_channel']))
     print(f'>>{member} join<<')
     await channel.send(f'>>{member} join<<')
 
 @bot.event
 async def on_member_remove(member):
     print(f'>>{member} leave<<')
-    channel = bot.get_channel(880437860761759784)
+    channel = bot.get_channel(int(jdata['Leave_channel']))
     await channel.send(f'>>{member} leave<<')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)} (ms)')
 
-bot.run("Token")
+bot.run(jdata['TOKEN'])
