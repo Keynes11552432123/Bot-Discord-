e0d0f5a01f908ae819f56f4f3b8ac529448db636
Keynes11552432123 <80331285+Keynes11552432123@users.noreply.github.com>
2022-02-25 22:21:06 +0800

    Version3.0

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+from msilib.schema import File
 import discord
 from discord.ext import commands
 import json
+import random
 
 with open ('setting.json',mode='r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
@@ -29,5 +31,16 @@ async def on_member_remove(member):
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)} (ms)')
+
+@bot.command()
+async def 傳送貓妖頻道圖片(ctx):
+    random_pic = random.choice(jdata['pic'])
+    pic = discord.File(random_pic)
+    await ctx.send(file = pic)
+
+@bot.command()
+async def 傳送總之就是很可愛圖片(ctx):
+    random_pic = random.choice(jdata['url_pic'])
+    await ctx.send(random_pic)
 
 bot.run(jdata['TOKEN'])
