9885d5db29fb1a9d3377dd27c0738c3232cd7044
Keynes11552432123 <80331285+Keynes11552432123@users.noreply.github.com>
2022-03-16 19:04:44 +0800

    keynes

--- newbot/bot.py
+++ newbot/bot.py
@@ -1,1 +1,10 @@
 import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix='?')
+
+@client.event
+async def on_reday():
+    print(">>Bot Is Online<<")
+
+client.run('OTUxNzk1OTU5NDgwODU2NjA2.YisrEA.xykQqyz5K4zH6uljvf_hZITAvE4')
